817656e96329cc978bf39a664c34e5ddbdcf817d
Himanshu Gangwar <gangwarsanu8@gmail.com>
2025-02-19 08:02:49 +0530

    add agent

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from phi.agent import Agent, RunResponse
 from phi.model.groq import Groq
 from dotenv import load_dotenv
+from phi.tools.yfinance import YFinanceTools
 import os
 load_dotenv()
 agent = Agent(
     model=Groq(id="llama-3.3-70b-versatile"),
-    markdown=True
+    tools = [YFinanceTools(stock_price=True,analyst_recommendations=True,stock_fundamentals = True)],
+    markdown=True,
+    show_tool_call = True,
+    instructions = ["use table to display data."]
 )
 
-agent.print_response("Share a 2 sentence horror story.")
+
+agent.print_response("summarize and compaire the stock price of SBI and HDFC")
